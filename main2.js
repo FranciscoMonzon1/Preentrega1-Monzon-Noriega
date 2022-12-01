9da30752fd516ff81863fd018f00a94c7c791dfb
Francisco Monzon <franmonzon13@hotmail.com>
2022-11-30 23:50:02 -0300

    Segunda pre-entrega

---
+++ main2.js
@@ -0,0 +1,23 @@
+
+/*const boton = document.querySelector('button');
+const generateAlert = () => {
+    alerta = alert('El boton funciona');
+}
+boton.addEventListener('click', generateAlert)*/
+
+//El proyecto consiste en sacar un promedio de goles a favor y 
+//goles en contra en la primera fase del grupo A en un mundial imaginario .
+// para ello primero generaremos un objeto que nos permitira sacar el promedio de los goles que tendra la 
+//siguiente forma :
+/* {
+golesAfavor:  [¨GF1, GF2, GF3 ]
+golesEnContra: [GC1, GC2, GC3]
+}
+luego calcularemos el promedio de estos goles y pondremos el resultado en un alert que le sale al usuario
+en pantalla
+
+ Seleccionamos cada input del formulario y lo guardamos en variables; */
+
+ const form = document.querySelector('form');
+ 
+
